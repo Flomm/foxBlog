190480be4f7d91d3ca31dcca2b23ce3e5562a9f3
Florian Baranya <florian.baranya@gmail.com>
2021-03-18 19:52:31 +0100

    Add types to scrollPostfile

--- Frontend/tsFiles/scrollToPost.ts
+++ Frontend/tsFiles/scrollToPost.ts
@@ -1,6 +1,6 @@
 export default function scrollToPost(post: HTMLDivElement): void {
-  const headerHeight = 50;
-  const screenHalf = window.screen.height / 2;
+  const headerHeight: number = 50;
+  const screenHalf: number = window.screen.height / 2;
   const y: number = post.getBoundingClientRect().top + window.scrollY - headerHeight - screenHalf;
   window.scroll({
     top: y,
