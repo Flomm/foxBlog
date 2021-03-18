549de1347fd77aa9313faf6478ba3ea4833e7acf
Florian Baranya <florian.baranya@gmail.com>
2021-03-18 19:52:11 +0100

    Add db insert

--- server.ts
+++ server.ts
@@ -1,6 +1,5 @@
 import Postable from './Backend/Postable';
 import * as express from 'express';
-import * as fs from 'fs';
 import { connection } from './Backend/sqlConnect';
 
 const app = express();
@@ -26,8 +25,7 @@ app.get('/', (req: express.Request, res: express.Response) => {
 app.get('/api/posts', (req: express.Request, res: express.Response) => {
   connection.query('SELECT author, title, content, date FROM posts', (err: Error, result) => {
     if (err) {
-      console.error(err);
-      return;
+      return console.error(err);
     }
     const posts: Postable[] = result;
     res.send(posts);
@@ -35,13 +33,12 @@ app.get('/api/posts', (req: express.Request, res: express.Response) => {
 });
 
 app.post('/api/addpost', (req: express.Request, res: express.Response) => {
-  try {
-    const newPost: Postable = req.body;
-    const blogFileArr: Postable[] = JSON.parse(fs.readFileSync('./assets/posts.json', 'utf8'));
-    blogFileArr.push(newPost);
-    fs.writeFileSync('./assets/posts.json', JSON.stringify(blogFileArr));
+  const newPost: Postable = req.body;
+  connection.query('INSERT INTO posts SET ?;', newPost, (err: Error, result) => {
+    if (err) {
+      res.status(404).send();
+      return console.error(err);
+    }
     res.status(202).send();
-  } catch {
-    res.status(404).send;
-  }
+  });
 });
