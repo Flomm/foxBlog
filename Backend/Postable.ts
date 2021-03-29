76954a7e4d3f5e05e9826d4be182fcbf8a3f25c9
Florian Baranya <florian.baranya@gmail.com>
2021-03-29 17:17:04 +0200

    Adjust interfaces for score, vote, user

--- Backend/Postable.ts
+++ Backend/Postable.ts
@@ -1,6 +1,10 @@
 export default interface Postable {
+  id: number;
   author: string;
   title: string;
   content: string;
   date: string;
+  score: number;
+  user?: string;
+  vote?: string;
 }
--- Frontend/tsFiles/Postable.ts
+++ Frontend/tsFiles/Postable.ts
@@ -1,6 +1,9 @@
 export default interface Postable {
+  id: number;
   author: string;
   title: string;
   content: string;
   date: string;
+  user?: string;
+  vote?: string;
 }
