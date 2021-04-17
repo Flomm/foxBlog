92b40ecb0250eb36c6932a9435b59d0c54a9886a
Florian Baranya <florian.baranya@gmail.com>
2021-04-17 20:29:29 +0200

    Adjust scroll-to height after post added

--- Frontend/tsFiles/functions/scrollToPost.ts
+++ Frontend/tsFiles/functions/scrollToPost.ts
@@ -1,7 +1,7 @@
 export default function scrollToPost(post: HTMLDivElement): void {
   const headerHeight: number = 50;
   const screenHalf: number = window.screen.height / 2;
-  const y: number = post.getBoundingClientRect().top + window.scrollY - headerHeight - screenHalf;
+  const y: number = post.getBoundingClientRect().bottom + window.scrollY - headerHeight - screenHalf;
   window.scroll({
     top: y,
     behavior: 'smooth',
